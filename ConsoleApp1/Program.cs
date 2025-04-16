cb66d44e0bc471b62aef592dca6d2a50b1cbd86a
Jhocep Verastegui <snkje123@gmail.com>
2025-04-16 17:25:44 -0500

    Project_2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            /*
             int a, b, c, disc;
             double x1, x2;
             Console.WriteLine("escriba el valor de a: ");
@@ -37,8 +38,50 @@ namespace ConsoleApp1
                 }else
                 {
                     Console.WriteLine("no tiene soluciones: ");
-                }
+                }*/
+            /*
+            int a, b, c, d, not;
+            Console.WriteLine("La primera nota es: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("La segunda nota es: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("La tercera nota es: ");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine("La cuarta nota es: ");
+            d = int.Parse(Console.ReadLine());
+            not = (a + b+ c+ d)/4;
+
+            if (not > 12)
+            {
+                Console.WriteLine("el alumno esta aprobado");
             }
+            else
+            {
+                Console.WriteLine("El alumno esta desaprobado");
+            }
+            */
+            double a, b, c, d, not;
+            Console.WriteLine("La primera nota es (10%): ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("La segunda nota es (20%): ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("La tercera nota es (30%): ");
+            c = double.Parse(Console.ReadLine());
+            Console.WriteLine("La cuarta nota es (40%): ");
+            d = double.Parse(Console.ReadLine());
+            not = (a * 0.1) + (b * 0.2) + (c * 0.3) + (d * 0.4);
+            Console.WriteLine("Promedio: " + not);
+            if (not >= 11.5)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("el alumno esta aprobado");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("El alumno esta desaprobado");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
 }
